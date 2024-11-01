c0e25e019f27220b0c5c6f92c7f95930fc384e67
Tácio Souza <110792078+taciosouzaoliveira@users.noreply.github.com>
2024-11-01 13:24:57 -0300

    Update firewall.sh

--- firewall.sh
+++ firewall.sh
@@ -1,103 +1,34 @@
 #!/bin/bash
 
+# Função para configurar regras de firewall locais
 local(){
 
-# Libera trafego com servidor Samba AD DC #
+    # Libera tráfego com o servidor Samba AD DC na interface bond0 para o IP específico
+    iptables -t filter -A INPUT -i bond0 -s 10.10.10.5 -j ACCEPT
+    iptables -t filter -A OUTPUT -o bond0 -d 10.10.10.5 -j ACCEPT
 
-iptables -t filter -A INPUT -i bond0 -s 10.10.10.5 -j ACCEPT
-iptables -t filter -A OUTPUT -o bond0 -d 10.10.10.5 -j ACCEPT
+    # Libera acesso remoto SSH externo na interface enp0s3
+    iptables -t filter -A INPUT -i enp0s3 -p tcp --dport 22 -j ACCEPT
+    iptables -t filter -A OUTPUT -o enp0s3 -p tcp --sport 22 -j ACCEPT
 
-# Libera acesso remoto SSH externo #
+    # Libera acesso SSH do servidor firewall para os servidores da rede 10.10.10.0/24 na interface bond0
+    iptables -t filter -A INPUT -i bond0 -p tcp --sport 22 -s 10.10.10.0/24 -j ACCEPT
+    iptables -t filter -A OUTPUT -o bond0 -p tcp --dport 22 -d 10.10.10.0/24 -j ACCEPT
 
-iptables -t filter -A INPUT -i enp0s3 -p tcp --dport 22 -j ACCEPT
-iptables -t filter -A OUTPUT -o enp0s3 -p tcp --sport 22 -j ACCEPT
+    # Libera portas 443 (HTTPS), 80 (HTTP), 53 (DNS), e 123 (NTP) na interface enp0s3 para o firewall
+    iptables -t filter -A INPUT -i enp0s3 -p tcp -m multiport --sports 80,443 -j ACCEPT
+    iptables -t filter -A INPUT -i enp0s3 -p udp -m multiport --sports 53,123 -j ACCEPT
+    iptables -t filter -A OUTPUT -o enp0s3 -p tcp -m multiport --dports 80,443 -j ACCEPT
+    iptables -t filter -A OUTPUT -o enp0s3 -p udp -m multiport --dports 53,123 -j ACCEPT
 
-# Libera acesso remoto SSH do servidor firewall para os servidores da infraestrutura #
+    # Libera conexões ICMP (ping) na interface enp0s3
+    iptables -t filter -A OUTPUT -o enp0s3 -p icmp --icmp-type 8 -d 0/0 -j ACCEPT
+    iptables -t filter -A INPUT -i enp0s3 -p icmp --icmp-type 0 -s 0/0 -j ACCEPT
 
-iptables -t filter -A INPUT -i bond0 -p tcp --sport 22 -s 10.10.10.0/24 -j ACCEPT
-iptables -t filter -A OUTPUT -o bond0 -p tcp --dport 22 -d 10.10.10.0/24 -j ACCEPT
+    # Libera tráfego na interface loopback (localhost)
+    iptables -t filter -A INPUT -i lo -j ACCEPT
+    iptables -t filter -A OUTPUT -o lo -j ACCEPT
 
-# Libera as portas 443, 80, 53 e 123 de conexões provindas do servidor firewall
-
-iptables -t filter -A INPUT -i enp0s3 -p tcp -m multiport --sports 80,443 -j ACCEPT
-iptables -t filter -A INPUT -i enp0s3 -p udp -m multiport --sports 53,123 -j ACCEPT
-iptables -t filter -A OUTPUT -o enp0s3 -p tcp -m multiport --dports 80,443 -j ACCEPT
-iptables -t filter -A OUTPUT -o enp0s3 -p udp -m multiport --dports 53,123 -j ACCEPT
-
-# Libera conexões para o protocolo icmp - ping
-
-iptables -t filter -A OUTPUT -o enp0s3 -p icmp --icmp-type 8 -d 0/0 -j ACCEPT
-iptables -t filter -A INPUT -i enp0s3 -p icmp --icmp-type 0 -s 0/0 -j ACCEPT
-
-# Libera trafego na interface loopback
-
-iptables -t filter -A INPUT -i lo -j ACCEPT
-iptables -t filter -A OUTPUT -o lo -j ACCEPT
-
-# Libera trafego ao ssh e proxy squid #
-
-iptables -t filter -A INPUT -i bond0 -p tcp -m multiport --dports 22,3128 -s 10.10.10.0/24 -j ACCEPT
-iptables -t filter -A OUTPUT -o bond0 -p tcp -m multiport --sports 22,3128 -d 10.10.10.0/24 -j ACCEPT
-
-}
-
-forwarding(){
-
-# Libera as portas 443, 80, 53 e 123 de conexões originadas da rede LAN 10.10.10.0/24 
-
-iptables -t filter -A FORWARD -i enp0s3 -p tcp -m multiport --sports 80,443 -d 10.10.10.0/24 -j ACCEPT
-iptables -t filter -A FORWARD -i enp0s3 -p udp -m multiport --sports 53,123 -d 10.10.10.0/24 -j ACCEPT
-iptables -t filter -A FORWARD -i enp0s3 -p tcp --sport 53 -d 10.10.10.0/24 -j ACCEPT
-iptables -t filter -A FORWARD -i bond0 -p tcp -m multiport --dports 80,443 -s 10.10.10.0/24 -j ACCEPT
-iptables -t filter -A FORWARD -i bond0 -p udp -m multiport --dports 53,123 -s 10.10.10.0/24 -j ACCEPT
-iptables -t filter -A FORWARD -i bond0 -p tcp --dport 53 -s 10.10.10.0/24 -j ACCEPT
-
-# Libera conexões para o protocolo icmp - ping
-
-iptables -t filter -A FORWARD -o enp0s3 -p icmp --icmp-type 8 -d 0/0 -s 10.10.10.0/24 -j ACCEPT
-iptables -t filter -A FORWARD -i enp0s3 -p icmp --icmp-type 0 -s 0/0 -d 10.10.10.0/24 -j ACCEPT
-
-
-}
-
-internet(){
-
-sysctl -w net.ipv4.ip_forward=1
-iptables -t nat -A POSTROUTING -s 10.10.10.0/24 -o enp0s3 -j MASQUERADE
-
-}
-
-default(){
-
-iptables -t filter -P INPUT DROP
-iptables -t filter -P OUTPUT DROP
-iptables -t filter -P FORWARD DROP
-
-}
-
-iniciar(){
-
-local
-forwarding
-default
-internet
-
-}
-
-parar(){
-
-iptables -t filter -P INPUT ACCEPT
-iptables -t filter -P OUTPUT ACCEPT
-iptables -t filter -P FORWARD ACCEPT
-iptables -t filter -F
-
-}
-
-
-case $1 in
-start|START|Start)iniciar;;
-stop|STOP|Stop)parar;;
-restart|RESTART|Restart)parar;iniciar;;
-listar)iptables -t filter -nvL;;
-*)echo "Execute o script com os parâmetros start ou stop ou restart";;
-esac
+    # Libera tráfego nas portas SSH e proxy Squid na interface bond0 para a rede 10.10.10.0/24
+    iptables -t filter -A INPUT -i bond0 -p tcp -m multiport --dports 22,3128 -s 10.10.10.0/24 -j ACCEPT
+    iptables -t filter -A OUTPUT -o bond0 -p tcp -m multiport --sports 22,3128 -d 10.10.10.
